f03504754a3662138d371d2a51779f7cfe6e82fb
RYTNIX-ABHAY <abhaypratap12340000@gmail.com>
2026-02-23 20:57:35 +0530

    custom setup complete (middlewares added)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-
+import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config({path : './env'})
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is listening on Port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("DataBase Connection Failed !!!" ,error);
+})
